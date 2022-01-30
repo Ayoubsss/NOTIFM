7f0c1208df2a8ef50bed30ff413dcdff07121404
Ayoub El Asry <ayoubsss@gmail.com>
2022-01-29 23:15:52 -0600

    Addition of Urbanist Font + Implementation

--- NOTIFM/NOTIFM/AssemblyInfo.cs
+++ NOTIFM/NOTIFM/AssemblyInfo.cs
@@ -1,3 +1,10 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+
+#region EmbeddedFont UrbanistFont
+[assembly: ExportFont("Urbanist-Regular.ttf", Alias ="UrbanistFont")]
+[assembly: ExportFont("Urbanist-Bold.ttf", Alias = "UrbanistFontBold")]
+#endregion
