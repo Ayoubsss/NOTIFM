7440d61b4409a543e678c2f009eaeb5cb53faf77
Ayoub El Asry <ayoubsss@gmail.com>
2022-02-25 19:05:10 -0600

    Addition of Firebase Auth service for Android

---
+++ NOTIFM/NOTIFM.Android/FirebaseAuthentication.cs
@@ -0,0 +1,69 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Auth;
+using NOTIFM.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NOTIFM.Droid
+{
+    public class FirebaseAuthentication : IAuthenticationService
+    {
+        public async Task<bool> CreateUser(string username, string email, string password)
+        {
+            var authResult = await Firebase.Auth.FirebaseAuth.Instance.CreateUserWithEmailAndPasswordAsync(email, password);
+
+            return await Task.FromResult(true);
+        }
+
+        public bool IsSignIn()
+        {
+            var user = Firebase.Auth.FirebaseAuth.Instance.CurrentUser;
+            return user != null;
+        }
+
+        public async Task<string> SignIn(string email, string password)
+        {
+            try
+            {
+                var user = await Firebase.Auth.FirebaseAuth.Instance.SignInWithEmailAndPasswordAsync(email, password);
+                var token = user.User.GetIdToken(true);
+                return token.Result.ToString();
+            }
+            catch (FirebaseAuthInvalidUserException e)
+            {
+                e.PrintStackTrace();
+                return string.Empty;
+            }
+            catch (FirebaseAuthInvalidCredentialsException e)
+            {
+                e.PrintStackTrace();
+                return string.Empty;
+            }
+        }
+
+        public async Task ResetPassword(string email)
+        {
+            await Firebase.Auth.FirebaseAuth.Instance.SendPasswordResetEmailAsync(email);
+        }
+
+        void IAuthenticationService.SignOut()
+        {
+            try
+            {
+                Firebase.Auth.FirebaseAuth.Instance.SignOut();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
--- NOTIFM/NOTIFM.Android/MainActivity.cs
+++ NOTIFM/NOTIFM.Android/MainActivity.cs
@@ -4,6 +4,9 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Firebase;
+using Xamarin.Forms;
+using NOTIFM.Common;
 
 namespace NOTIFM.Droid
 {
@@ -16,6 +19,9 @@ namespace NOTIFM.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            DependencyService.Register<IAuthenticationService, FirebaseAuthentication>();
+            FirebaseApp.InitializeApp(Application.ApplicationContext);
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
