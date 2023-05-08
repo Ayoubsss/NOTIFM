896cd5587c3503a90d4f569e9c7bbfca35fef686
Ayoub El Asry <ayoubsss@gmail.com>
2023-05-08 18:10:52 -0500

    Add project files.

---
+++ NOTIFM/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace NOTIFM;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ NOTIFM/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NOTIFM;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
